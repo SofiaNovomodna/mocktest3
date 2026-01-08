e15f8e4920c1dc6760d95a80c89872448fa82632
SofiaNovomodna <242595@student.uek.krakow.pl>
2026-01-08 19:11:09 +0100

    Update p12.py

--- p12.py
+++ p12.py
@@ -2,7 +2,7 @@ def f(date):
     import re
     pat = '^\d{4}-\d{2}-\d{2}$'
     l2 = []
-    date_list = date.split(', ')
+    date_list = date.split(',')
     for i in date_list:
         ch = re.match(pat,i)
         if ch != None:
@@ -10,4 +10,5 @@ def f(date):
     return l2
 
 dates = "2021-1-3, 05/12/2024, 1998-12-11, 9 maj 2007, 2001-12-07, 15-09-2011"
+
 print(f(dates))# returns ["1998-12-11","2001-12-07"]
